4b2e2804d1b44ac8a4a009a56f3c427e2cd744e8
shrijitmajumder <84119964+shrijitmajumder@users.noreply.github.com>
2021-05-17 17:58:05 +0530

    Add files via upload

---
+++ Day3/EmailCheck.java
@@ -0,0 +1,23 @@
+// 14)Write a java program to check given email contains @ and .?
+
+import java.util.*;
+public class EmailCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the email : ");
+		String email=sc.next();
+		if(email.contains("@") && email.contains("."))
+			System.out.println("Email contains both '@' and '.'");
+		else if(email.contains("@"))
+			System.out.println("Email contains '@' but not '.'");
+		else if(email.contains("."))
+			System.out.println("Email contains '.' but not '@'");
+		else
+			System.out.println("Email does not contain both '@' and '.'");
+		
+		sc.close();
+	}
+
+}
---
+++ Day3/UpperCaseFront.java
@@ -0,0 +1,28 @@
+// 13)Write a java program to read a string and move all uppercase characters to front?
+
+import java.util.*;
+
+public class UpperCaseFront {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the string : ");
+		String string=sc.next();
+		String newstr="";
+		for(int i=0;i<string.length();i++) {
+			if(string.charAt(i)>=65 && string.charAt(i)<=90) {
+				newstr+=string.charAt(i);
+			}
+		}
+		for(int i=0;i<string.length();i++) {
+			if(!(string.charAt(i)>=65 && string.charAt(i)<=90)) {
+				newstr+=string.charAt(i);
+			}
+		}
+		System.out.println("Output String : "+newstr);
+		
+		sc.close();
+	}
+
+}
