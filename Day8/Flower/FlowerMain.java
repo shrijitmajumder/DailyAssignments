aad9f4ebc8a0834c44dff8ba6e17a11a99bc7281
shrijitmajumder <84119964+shrijitmajumder@users.noreply.github.com>
2021-05-24 18:28:03 +0530

    Create FlowerMain.java

---
+++ Day8/Flower/FlowerMain.java
@@ -0,0 +1,89 @@
+package com.capgemini.flower;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class FlowerMain {
+	
+	static HashSet<Flower> flowerList=new HashSet<Flower>();
+	static Iterator<Flower> fitr = flowerList.iterator();
+	static Flower flower[];
+	static int num=0;
+	public static void read() {
+		Scanner sm=new Scanner(System.in);
+		System.out.print("Enter the number of flowers you want to add : ");
+		int n=sm.nextInt();
+		num=num+n;
+		flower=new Flower[num];
+		for(int i=0;i<n;i++) {
+			System.out.println("Enter Details of Flower "+(i+1));
+			System.out.print("Enter flower id : ");
+			int fid=sm.nextInt();
+			System.out.print("Enter flower name : ");
+			sm.nextLine();
+			String fname=sm.nextLine();
+			System.out.print("Enter price per kg : ");
+			double pricePerKg=sm.nextDouble();
+			System.out.print("Enter quantity : ");
+			float qty=sm.nextFloat();
+			flower[i]=new Flower(fid,fname,pricePerKg,qty);
+			flowerList.add(flower[i]);
+		}
+	}
+	
+	public static void displayFlowers() {
+		fitr = flowerList.iterator();
+		if(flowerList.size()<1) {
+		System.out.println("\nDisplay not possible since no flower is added...");
+	}else {
+		int k=1;
+			System.out.println("The flowers are : ");
+			while(fitr.hasNext()) {
+				System.out.println(fitr.next().toString(k));
+				k++;
+			}
+	}
+	}
+
+	public static void remove() {
+		if(flowerList.size()<1) {
+			System.out.println("\nRemoval not possible since no flower is added...");
+		}else {
+			for(int i=0;i<flowerList.size();i++) {
+				if(flower[i].getQty()<=0) {
+					flowerList.remove(flower[i]);
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sm=new Scanner(System.in);
+		int choice;
+		char ch='y';
+		
+		while(ch=='y' || ch=='Y') {
+			System.out.print("\n1. Create List of Flowers\n2. Display Flower List\n3. Remove Flower where quantity <= 0\n\nEnter your choice : ");
+			choice=sm.nextInt();
+			switch(choice) {
+				case 1 : read();
+						break;
+				case 2 : displayFlowers();
+						
+						break;
+				case 3: 
+				
+					remove();
+					
+				break;
+			}
+			System.out.println("Do you want to continue?(y/n) ");
+			ch=sm.next().trim().charAt(0);
+		}
+		
+		sm.close();
+	}
+
+}
